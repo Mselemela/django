a88ce366cf89802378508eec0dd0222f690187f6
Mpho <mselemela10@gmail.com>
2025-08-02 06:35:22 +0200

    django app

---
+++ django-polls/docs/conf.py
@@ -0,0 +1,9 @@
+extensions = [
+    'sphinx.ext.intersphinx',
+    # other extensions...
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'django': ('https://docs.djangoproject.com/en/stable/', 'https://docs.djangoproject.com/en/stable/_objects/'),
+}
